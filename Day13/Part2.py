cb01c519ee7a83435aa56bd07ee2a79ec0bbf73c
steveisaac <steve.isaac@live.co.uk>
2022-12-13 23:07:34 +0000

    one-line part2

---
+++ Day13/Part2.py
@@ -0,0 +1,4 @@
+from Input import inp
+from math import prod
+
+print(prod(len(["ULTRAGIGAMEGASUPERDUPERPLACEHOLDER" for packet in packets if (lambda f,x,y,finished:f(f,x,y, finished))(lambda f,x,y,finished: any(res) if (res:=[a < b if type(a) is type(b) is int and finished.append(1) == None else f(f, [a] if type(a) is int else a, [b] if type(b) is int else b, finished) for a,b in zip(x,y) if not finished and a != b]) and finished else len(x)!=len(y) and finished.append(1) == None and len(x) < len(y),packet,divider,[])]) for divider, packets in zip([[[2]], [[6]]],[[eval(x) for x in inp.split()]+[[0],[3]]]*2)))
